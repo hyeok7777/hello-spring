2041020e80979b7611e928fd93d4e18d7b3a4b0e
오진혁 <hyeok7777@gmail.com>
2021-09-04 21:23:27 +0900

    JDBC

--- src/main/java/com/study/hellospring/SpringConfiguration.java
+++ src/main/java/com/study/hellospring/SpringConfiguration.java
@@ -1,14 +1,24 @@
 package com.study.hellospring;
 
 
+import com.study.hellospring.repository.JdbcMemberRepository;
 import com.study.hellospring.repository.MemberRepository;
-import com.study.hellospring.repository.MemoryMemberRepository;
 import com.study.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfiguration {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfiguration(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +27,6 @@ public class SpringConfiguration {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
---
+++ src/test/java/com/study/hellospring/repository/MemberServiceIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.study.hellospring.repository;
+
+
+import com.study.hellospring.domain.Member;
+import com.study.hellospring.service.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test
+    public void 회원가입() throws Exception {
+        //Given
+        Member member = new Member();
+        member.setName("hello");
+        //When
+        Long saveId = memberService.join(member); //Then
+        Member findMember = memberRepository.findbyId(saveId).get();
+        assertEquals(member.getName(), findMember.getName());
+    }
+}
